c33687c27affba5d4355593da7d24a569db81d1c
nt-leoleo <pederneraleonardo729@gmail.com>
2025-08-24 16:50:38 -0300

    Se agregó un método de recibir emails mediante PHP y Formspree

---
+++ sendMail.php
@@ -0,0 +1,18 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $correo = htmlspecialchars($_POST['correo']);
+    $mensaje = htmlspecialchars($_POST['mensaje']);
+
+    $to = "pederneraleonardo.tec@gmail.com";
+    $subject = "Nuevo mensaje desde tu web ESTUDIO JURÍDICO";
+    $body = "Nombre: $nombre\nCorreo: $correo\nMensaje: $mensaje";
+    $headers = "From: $correo\r\nReply-To: $correo";
+
+    if(mail($to, $subject, $body, $headers)){
+        echo "¡Mensaje enviado correctamente! Gracias por contactarte.";
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
+}
+?>
